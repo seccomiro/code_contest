671aeb8875b275c03209ae6c70e77f2244579de6
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-05-13 13:50:06 -0300

    Rename app to CodeContest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "contest_production"
+  # config.active_job.queue_name_prefix = "code_contest_production"
 
   config.action_mailer.perform_caching = false
 
